89ce7e81ad379dca1f20822dec76f97bdc1bdbac
Majok-Bol <bolmajok001@gmail.com>
2025-04-02 20:35:34 +0300

    added background color to titlebar

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('item extent builder'),centerTitle: true,),
+        appBar: AppBar(title: Text('item extent builder'),centerTitle: true,backgroundColor: Colors.purple,),
       ),
     );
   }
